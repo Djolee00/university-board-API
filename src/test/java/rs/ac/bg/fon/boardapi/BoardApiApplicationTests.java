21f407a0d6ace406920258ab6cea20af0614c8ee
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-04-03 17:03:41 +0200

    Initial commit

---
+++ src/test/java/rs/ac/bg/fon/boardapi/BoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.boardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
